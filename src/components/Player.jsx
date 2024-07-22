4fcc08172e51b86e84dc372cd7101b3487ed1afe
Samson Badejo <zwitsam8@gmail.com>
2024-07-22 09:38:42 +0100

    reactjs 2

---
+++ src/components/Player.jsx
@@ -0,0 +1,39 @@
+// src/components/Player.js
+import React from 'react';
+import { Card } from 'react-bootstrap';
+import PropTypes from 'prop-types';
+
+const Player = ({ name, team, nationality, jerseyNumber, age, imageUrl }) => {
+  return (
+    <Card className="player-card" style={{ width: '18rem', margin: '1rem', transition: 'transform 0.3s, box-shadow 0.3s', borderRadius: '15px', overflow: 'hidden' }}>
+      <Card.Img variant="top" src={imageUrl} style={{ height: '250px', objectFit: 'cover' }} />
+      <Card.Body>
+        <Card.Title>{name}</Card.Title>
+        <Card.Text>Team: {team}</Card.Text>
+        <Card.Text>Nationality: {nationality}</Card.Text>
+        <Card.Text>Jersey Number: {jerseyNumber}</Card.Text>
+        <Card.Text>Age: {age}</Card.Text>
+      </Card.Body>
+    </Card>
+  );
+};
+
+Player.propTypes = {
+  name: PropTypes.string,
+  team: PropTypes.string,
+  nationality: PropTypes.string,
+  jerseyNumber: PropTypes.number,
+  age: PropTypes.number,
+  imageUrl: PropTypes.string
+};
+
+Player.defaultProps = {
+  name: "Unknown Player",
+  team: "Unknown Team",
+  nationality: "Unknown Nationality",
+  jerseyNumber: 0,
+  age: 0,
+  imageUrl: "https://via.placeholder.com/150"
+};
+
+export default Player;
---
+++ src/players.js
@@ -0,0 +1,38 @@
+// src/players.js
+const players = [
+    {
+      name: "Lionel Messi",
+      team: "Inter Miami CF",
+      nationality: "Argentinian",
+      jerseyNumber: 10,
+      age: 36,
+      imageUrl: "https://4kwallpapers.com/images/wallpapers/lionel-messi-football-player-argentinian-fc-barcelona-2048x2048-3265.jpg"
+    },
+    {
+      name: "Cristiano Ronaldo",
+      team: "Al-Nassr",
+      nationality: "Portuguese",
+      jerseyNumber: 7,
+      age: 38,
+      imageUrl: "https://i.pinimg.com/originals/3f/fe/bf/3ffebf9cb953a5eceb48bfd2d54db8a2.jpg"
+    },
+    {
+      name: "Neymar Jr",
+      team: "Al-Hilal",
+      nationality: "Brazilian",
+      jerseyNumber: 10,
+      age: 31,
+      imageUrl: "https://i.pinimg.com/originals/4e/8e/4e/4e8e4ed2f57a7cca1d2ca443311487f2.jpg"
+    },
+    {
+      name: "Kylian Mbappe",
+      team: "Paris Saint-Germain",
+      nationality: "French",
+      jerseyNumber: 7,
+      age: 25,
+      imageUrl: "https://wallpapers.com/images/hd/kylian-mbappe-4k-forward-player-3sfhaiar7ev2k32f.jpg"
+    }
+  ];
+  
+  export default players;
+  
